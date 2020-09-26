9b0dd93ac60f4c187bb7928b26aaffa7ce1e1c45
sullie-dev <eoghan@sullie.dev>
2020-09-26 15:31:23 +0100

    Added additional functionality

--- assetts/js/todo.js
+++ assetts/js/todo.js
@@ -1,23 +1,59 @@
+var editing = false;
+
 //Check off specific todo by clicking
-$('ul').on("click", "li", function () {
-    $(this).toggleClass('completed');
+$("ul").on("click", "li .todo", function () {
+  $(this).toggleClass("completed");
 });
+
 // Remove todos
-$('ul').on("click", "li .delete", function (event) {
-    $(this).parent().fadeOut(500, function () {
-        $(this).remove();
-    })
-    event.stopPropagation();
-})
+$("ul").on("click", "li .delete", function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
 
-$("input[type='text']").keypress(function (event) {
-    if (event.which === 13) {
-        var toDoNew = $(this).val()
-        $('ul').append("<li>" + "<span class='delete'><i class='fa fa-trash'></i>  </span>" + toDoNew + "</li>")
-        $(this).val("");
+// edit todos
+$("ul").on("click", "li .edit", function (event) {
+  editing = true;
+  toDoText = $(this).parent();
+  $("#toDotxt").val($(this).parent().text());
+  $("input[type='text']").keypress(function (event) {
+    if (event.which === 13 && editing === true) {
+      var toDoUpdated = $(this).val();
+      toDoText.html(
+        "<span class='delete'><i class='fa fa-trash'></i></span> " +
+          "<span class='todo'>" +
+          toDoUpdated +
+          "</span>" +
+          "<span class='edit'><i class='fa fa-edit'></i></span>" +
+          "</li>"
+      );
+      $(this).val("");
+      editing = false;
     }
-})
+  });
+});
 
-$('.fa-plus').click(function () {
-    $("input[type='text']").fadeToggle();
-})
+//input todo
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13 && editing === false) {
+    var toDoNew = $(this).val();
+    $("ul").append(
+      "<li>" +
+        "<span class='delete'><i class='fa fa-trash'></i></span> " +
+        "<span class='todo'>" +
+        toDoNew +
+        "</span>" +
+        "<span class='edit'><i class='fa fa-edit'></i></span>" +
+        "</li>"
+    );
+    $(this).val("");
+  }
+});
+
+$(".fa-plus").click(function () {
+  $("input[type='text']").fadeToggle();
+});
